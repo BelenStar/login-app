d089809bb29c3d4b3b4d02ded96b6caf104af387
Nellyannis Ramirez <nfrv04@gmail.com>
2023-04-19 03:17:32 -0400

    Create base ui sign up form

--- lib/src/pages/register.dart
+++ lib/src/pages/register.dart
@@ -1,20 +1,54 @@
 import 'package:flutter/material.dart';
 
+import '../styles/styles.dart';
+
 class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
 
   @override
-  createState() => _registerPageState();
+  createState() => _RegisterPageState();
 }
 
-class _registerPageState extends State<RegisterPage> {
+class _RegisterPageState extends State<RegisterPage> {
+  final email = TextEditingController();
+  final password = TextEditingController();
+  final confirmation = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Center(
-      child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [Text('hi there')]),
+      child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        Icon(
+          Icons.account_circle,
+          size: 120.0,
+          color: Color.fromARGB(255, 69, 69, 69),
+        ),
+        Text(
+          'Join us',
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+        ),
+        TextField(
+          controller: email,
+          keyboardType: TextInputType.emailAddress,
+          decoration: ThemeStyle.emailFormField,
+        ),
+        TextField(
+          controller: password,
+          obscureText: true,
+          enableSuggestions: false,
+          autocorrect: false,
+          decoration: ThemeStyle.passwordFormField,
+        ),
+        TextField(
+          controller: confirmation,
+          obscureText: true,
+          enableSuggestions: false,
+          autocorrect: false,
+          decoration: ThemeStyle.passwordFormField,
+        ),
+        ElevatedButton(onPressed: null, child: null)
+      ]),
     ));
   }
 }
