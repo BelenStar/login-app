d1b8e0ead5425033d7996efcbd085468424a774e
Nellyannis Ramirez <nfrv04@gmail.com>
2023-04-21 02:01:14 -0400

    fix design and add async function sign up firebase

--- lib/src/pages/register.dart
+++ lib/src/pages/register.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 import '../styles/styles.dart';
@@ -14,40 +15,65 @@ class _RegisterPageState extends State<RegisterPage> {
   final password = TextEditingController();
   final confirmation = TextEditingController();
 
+  Future signUp() async {
+    await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: email.text, password: password.text);
+  }
+
+  @override
+  void dispose() {
+    email.dispose();
+    password.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Center(
       child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-        Icon(
+        const Icon(
           Icons.account_circle,
           size: 120.0,
           color: Color.fromARGB(255, 69, 69, 69),
         ),
-        Text(
+        const Text(
           'Join us',
           style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
         ),
-        TextField(
-          controller: email,
-          keyboardType: TextInputType.emailAddress,
-          decoration: ThemeStyle.emailFormField,
+        const SizedBox(height: 20),
+        SizedBox(
+          width: 275,
+          child: TextField(
+            controller: email,
+            keyboardType: TextInputType.emailAddress,
+            decoration: ThemeStyle.emailFormField,
+          ),
         ),
-        TextField(
-          controller: password,
-          obscureText: true,
-          enableSuggestions: false,
-          autocorrect: false,
-          decoration: ThemeStyle.passwordFormField,
+        const SizedBox(height: 20),
+        SizedBox(
+          width: 275,
+          child: TextField(
+            controller: password,
+            obscureText: true,
+            enableSuggestions: false,
+            autocorrect: false,
+            decoration: ThemeStyle.passwordFormField,
+          ),
         ),
-        TextField(
-          controller: confirmation,
-          obscureText: true,
-          enableSuggestions: false,
-          autocorrect: false,
-          decoration: ThemeStyle.passwordFormField,
-        ),
-        ElevatedButton(onPressed: null, child: null)
+        /*  const SizedBox(height: 20),
+        SizedBox(
+          width: 275,
+          child: TextField(
+            controller: confirmation,
+            obscureText: true,
+            enableSuggestions: false,
+            autocorrect: false,
+            decoration: ThemeStyle.passwordConfirmationFormField,
+          ),
+        ), */
+        const SizedBox(height: 25),
+        ElevatedButton(onPressed: signUp, child: const Text('Sign Up'))
       ]),
     ));
   }
