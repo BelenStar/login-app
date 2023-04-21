a4dff6729dfe45bd106f9f2659e515e3ea4215dc
Nellyannis Ramirez <nfrv04@gmail.com>
2023-04-21 02:04:46 -0400

    style changes

--- lib/src/styles/styles.dart
+++ lib/src/styles/styles.dart
@@ -31,4 +31,19 @@ abstract class ThemeStyle {
       ),
       filled: true,
       fillColor: Color.fromARGB(255, 232, 232, 235));
+
+  static const InputDecoration passwordConfirmationFormField = InputDecoration(
+      //labelText: 'Password',
+      //  floatingLabelStyle: TextStyle(color: Color.fromARGB(255, 12, 58, 95)),
+      hintText: 'Confirm Password',
+      enabledBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: Color.fromARGB(255, 77, 77, 77)),
+        borderRadius: BorderRadius.all(Radius.circular(15.5)),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: Color.fromARGB(255, 161, 52, 229)),
+        borderRadius: BorderRadius.all(Radius.circular(15.5)),
+      ),
+      filled: true,
+      fillColor: Color.fromARGB(255, 232, 232, 235));
 }
